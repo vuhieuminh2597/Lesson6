2f1d8ead7b51bd97657e355470763e86e3ef438a
vuhieuminh2597 <vuhieuminh2597@gmial.com>
2023-10-13 22:06:09 +0700

    update

--- Week2/src/view/Main.java
+++ Week2/src/view/Main.java
@@ -1,7 +1,12 @@
 package view;
 
+import model.Company;
+import model.Manager;
+import model.SuperManager;
+
 public class Main {
     public static void main(String[] args) {
-
+        SuperManager manager = new SuperManager();
+        Company company = new Company();
     }
 }
