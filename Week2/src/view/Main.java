991a975a9b67c104243acd4502dd7d94498705c6
vuhieuminh2597 <vuhieuminh2597@gmial.com>
2023-10-13 22:07:40 +0700

    update

--- Week2/src/view/Main.java
+++ Week2/src/view/Main.java
@@ -5,9 +5,5 @@ import model.SuperManager;
 
 public class Main {
     public static void main(String[] args) {
-        SuperManager manager = new SuperManager();
-        Company company = new Company();
-        System.out.println(manager.toString());
-        System.out.println(manager.toString());
     }
 }
